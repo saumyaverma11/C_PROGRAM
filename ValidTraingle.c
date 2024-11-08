ecab989bb3c5a6e5aa67f993571dcf7f828e870b
Saumya Verma <149528902+saumyaverma11@users.noreply.github.com>
2024-11-08 18:04:59 +0530

    Update ValidTraingle.c

--- ValidTraingle.c
+++ ValidTraingle.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
 void main(){
-  int 
+  int s1,s2,s3;
+  printf("enter sides of traingle:"):
+    scanf("%d%d%d",&s1,&s2,&s3);
+  if((s1+s2+s3) == 180)
+  {
+    printf("this is traingle:");
+}
+else{
+  printf("this is not traingle");
+}
+getch();
 }
